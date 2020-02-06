c4498c2cad3ca3e63d37f12ea3913d5bf5d4f081
Saif Ulla Baig S <34671@ITCINFOTECH.COM>
2020-02-06 17:23:57 +0530

    pushing boilerplate

---
+++ bookup-webapp/src/main/java/com/bookup/webapp/BookupWebappApplication.java
@@ -0,0 +1,13 @@
+package com.bookup.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookupWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookupWebappApplication.class, args);
+	}
+
+}
