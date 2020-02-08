1a0a540474540bcf20d7e3d26d2b653f9c2071aa
Pooja Shantaram Pawar <34567@ITCINFOTECH.COM>
2020-02-08 13:27:37 +0530

    added autofill field in search component

--- bookup-webapp/bookup-angular/src/app/search/search.component.ts
+++ bookup-webapp/bookup-angular/src/app/search/search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -12,6 +12,22 @@ export class SearchComponent implements OnInit {
   ngOnInit() {
     this.TripData = sessionStorage.getItem('tripData');
     console.log('Trip data from dashboard::',this.TripData);
+    console.log('city----->', JSON.parse(this.TripData))
+    const tripInfo=JSON.parse(this.TripData);
+    console.log('dest-->',tripInfo.city);
+    this.booking_form.controls['city'].setValue(tripInfo.city);
+    this.booking_form.controls['distance'].setValue(tripInfo.distance.toFixed(2));
+    this.booking_form.controls['tripDate'].setValue(tripInfo.tripDate);
+    this.booking_form.controls['timeSlot'].setValue(tripInfo.timeSlot);
+
   }
+  booking_form = new FormGroup({
+    city: new FormControl({value:'', disabled: true},),
+    tripDate:new FormControl({value:'', disabled: true},),
+    timeSlot:new FormControl({value:'', disabled: true},),
+    city_from: new FormControl({value:'Banglore', disabled: true},),
+    distance:new FormControl({value:'', disabled: true},),
+  });
+
 
 }
